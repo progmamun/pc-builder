70df1b82ce955f3427013e395e1cead1c1c903f1
progmamun <mamun333089@gmail.com>
2023-07-29 09:52:35 +0600

    banner

---
+++ src/components/UI/Banner.js
@@ -0,0 +1,49 @@
+import { Carousel } from "antd";
+import Image from "next/image";
+import styles from "@/styles/Banner.module.css";
+
+function Banner() {
+  return (
+    <Carousel effect="fade" autoplay>
+      <div className={styles.imageContainer}>
+        <h3>CyberPowerPC Gamer Xtreme VR</h3>
+        <Image
+          src="https://m.media-amazon.com/images/I/71Lczneb0VL._AC_SL1500_.jpg"
+          layout="fill"
+          responsive
+          className={styles.image}
+          alt="banner img"
+        />
+      </div>
+      <div className={styles.imageContainer}>
+        <h3>Thermaltake Glacier 360 Liquid</h3>
+        <Image
+          src="https://m.media-amazon.com/images/I/61cXu9yGldL._AC_SL1200_.jpg"
+          layout="fill"
+          className={styles.image}
+          alt="banner img"
+        />
+      </div>
+      <div className={styles.imageContainer}>
+        <h3>Skytech Archangel Gaming Computer</h3>
+        <Image
+          src="https://m.media-amazon.com/images/I/81gqqKQVkAL._AC_SL1500_.jpg"
+          layout="fill"
+          className={styles.image}
+          alt="banner img"
+        />
+      </div>
+      <div className={styles.imageContainer}>
+        <h3>Skytech Azure Gaming PC Desktop</h3>
+        <Image
+          src="https://m.media-amazon.com/images/I/91CnBmI3G8L._AC_SL1500_.jpg"
+          layout="fill"
+          className={styles.image}
+          alt="banner img"
+        />
+      </div>
+    </Carousel>
+  );
+}
+
+export default Banner;
